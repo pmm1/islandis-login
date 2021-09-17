3d3899cdc8827aafacbfe79d1b7c3188fef117e0
Patrekur M. Magnusson <patrekur@myntkaup.is>
2021-09-17 16:46:19 +0000

    minor

--- index.d.ts
+++ index.d.ts
@@ -1,54 +1,54 @@
-declare module '@island.is/login' {
-  export type ErrorId =
-    | 'INVALID-TOKEN-XML'
-    | 'CERTIFICATE-INVALID'
-    | 'AUDIENCEURL-MISSING'
-    | 'LOGIN-REQUEST-EXPIRED'
-    | 'AUDIENCEURL-NOT-MATCHING';
+declare module "@islandis-login" {
+    export type ErrorId =
+        | "INVALID-TOKEN-XML"
+        | "CERTIFICATE-INVALID"
+        | "AUDIENCEURL-MISSING"
+        | "LOGIN-REQUEST-EXPIRED"
+        | "AUDIENCEURL-NOT-MATCHING";
 
-  export interface VerifyUser {
-    kennitala: string;
-    mobile: string;
-    fullname: string;
-    ip: string;
-    userAgent: string;
-    destinationSSN: string;
-    authId: string;
-    authenticationMethod: string;
-  }
+    export interface VerifyUser {
+        kennitala: string;
+        mobile: string;
+        fullname: string;
+        ip: string;
+        userAgent: string;
+        destinationSSN: string;
+        authId: string;
+        authenticationMethod: string;
+    }
 
-  export interface VerifyExtra {
-    destination: string;
-    audienceUrl: string;
-    dates: {
-      notBefore: Date;
-      notOnOrAfter: Date;
-    };
-  }
+    export interface VerifyExtra {
+        destination: string;
+        audienceUrl: string;
+        dates: {
+            notBefore: Date;
+            notOnOrAfter: Date;
+        };
+    }
 
-  export interface VerifyError {
-    id: ErrorId;
-    reason: string;
-  }
+    export interface VerifyError {
+        id: ErrorId;
+        reason: string;
+    }
 
-  export interface VerifyResult {
-    user?: VerifyUser;
-    extra?: VerifyExtra;
-    error?: VerifyError;
-  }
+    export interface VerifyResult {
+        user?: VerifyUser;
+        extra?: VerifyExtra;
+        error?: VerifyError;
+    }
 
-  class IslandisLogin {
-    constructor({
-      verifyDates,
-      audienceUrl
-    }: {
-      verifyDates?: boolean;
-      audienceUrl: string | null;
-      certificatePath?: string;
-    })
+    class IslandisLogin {
+        constructor({
+            verifyDates,
+            audienceUrl,
+        }: {
+            verifyDates?: boolean;
+            audienceUrl: string | null;
+            certificatePath?: string;
+        });
 
-    verify(token: string): Promise<VerifyResult>;
-  }
+        verify(token: string): Promise<VerifyResult>;
+    }
 
-  export default IslandisLogin;
+    export default IslandisLogin;
 }
